fc429e9ba78abbc170ee5115381e72a2672760de
tomiswinner <none>
2025-01-05 23:42:37 +0900

    ok

--- src/app/api/auth/reg-psky/route.ts
+++ src/app/api/auth/reg-psky/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-// 
+// api route の場合、default export は許可されていない
 export async function GET() {
   console.log("GET is called");
   return NextResponse.json({ message: "Hello World" });
